2885bd051596d96f88cb97221c179139ffa91f17
huhn511 <sehe89@gmail.com>
2021-05-22 23:55:46 +0200

    fix base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/trees-landingpage/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
